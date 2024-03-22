0010e22ab86428660d894a127c555541a805b0c4
Zhang-Bocheng <854528079@qq.com>
2024-03-22 15:16:01 +0800

    修改多层查询areaID为UUID，解决前端echarts结点唯一bug

--- knowledgegraph-admin/src/main/java/com/knowledgegraph/web/controller/neo4j/OrganizationController.java
+++ knowledgegraph-admin/src/main/java/com/knowledgegraph/web/controller/neo4j/OrganizationController.java
@@ -2,16 +2,19 @@ package com.knowledgegraph.web.controller.neo4j;
 
 import com.knowledgegraph.common.core.domain.AjaxResult;
 import com.knowledgegraph.neo4j.pojo.Coagreement;
+import com.knowledgegraph.neo4j.pojo.Organization;
+import com.knowledgegraph.neo4j.result.dto.OrganizationDto;
 import com.knowledgegraph.neo4j.result.vo.CoagreementVO;
 import com.knowledgegraph.neo4j.service.ICoagreementService;
 import com.knowledgegraph.neo4j.service.IExpertService;
 import com.knowledgegraph.neo4j.service.IOrganizationService;
-import com.sun.xml.internal.bind.v2.TODO;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -27,12 +30,11 @@ import java.util.List;
 @RestController
 @RequestMapping("/neo4j/organization")
 @Api(value = "以组织机构为单位的管理接口", tags = "以组织机构为单位的管理接口")
-public class OrganizationBasedController {
+public class OrganizationController {
     @Autowired
     IOrganizationService iOrganizationService;
     @Autowired
     private IExpertService iExpertService;
-
     @Autowired
     private ICoagreementService iCoagreementService;
 
@@ -47,8 +49,6 @@ public class OrganizationBasedController {
         return AjaxResult.success(iOrganizationService.queryExperts(orgName, relationship));
     }
 
-
-    //TODO:增加图谱的层数
     @ApiOperation("查询以xx机构为单位，关联的专家及其研究方向、论文的知识图谱")
     @GetMapping("/query")
     public AjaxResult queryGraph(@RequestParam("orgName") String orgName,
@@ -58,8 +58,6 @@ public class OrganizationBasedController {
         }
         return AjaxResult.success(iOrganizationService.queryGraph(orgName, relationship));
     }
-
-
 
 
     @ApiOperation("查询xx机构,类型为框架协议/学生交流/学术交流的协议")
@@ -82,5 +80,34 @@ public class OrganizationBasedController {
         }
         return AjaxResult.success(voList);
     }
+
+    @ApiOperation("增加机构结点")
+    @PostMapping()
+    public AjaxResult createOrganization(@RequestBody OrganizationDto organizationDto){
+        Organization organization = new Organization();
+        BeanUtils.copyProperties(organizationDto, organization);
+        organization.setCreateTime(LocalDateTime.now());
+        organization.setUpdateTime(LocalDateTime.now());
+        organization.setDisabled(0);
+
+        boolean save = iOrganizationService.save(organization);
+        if(save == true){
+            return AjaxResult.success("增加机构结点成功");
+        }else {
+            return AjaxResult.error("增加机构结点失败");
+        }
+    }
+
+//    @ApiOperation("删除机构结点")
+//    @DeleteMapping("/{organizationId}")
+//    public AjaxResult deleteOrganization(@PathVariable Long organizationId){
+//        boolean b = iOrganizationService.removeById(organizationId);
+//        if(b == true){
+//            return AjaxResult.success("删除机构结点成功");
+//        }else {
+//            return AjaxResult.error("删除机构结点失败");
+//        }
+//    }
+
 
 }
